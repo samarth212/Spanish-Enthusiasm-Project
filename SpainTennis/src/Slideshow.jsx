39e8d73b57f4bdf79efb82ca3f84af2d750d774f
Samarth <samarth212@gmail.com>
2024-05-26 21:53:06 -0400

    autoslide

--- SpainTennis/src/Slideshow.jsx
+++ SpainTennis/src/Slideshow.jsx
@@ -1,5 +1,5 @@
 import "./slideshow.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SlideShow = ({imageUrls}) => {
 
@@ -21,10 +21,28 @@ const SlideShow = ({imageUrls}) => {
         })
     }
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setImageIndex(index => {
+                if (index == imageUrls.length - 1) return 0
+                return index + 1
+            })
+        }, 4000);
+ 
+        return () => clearInterval(interval);
+    }, []);
+
 
     return (
+
+        
+
         <div style={{width:"1000px", height:"500px", position:"relative"}}>
-            <img src={imageUrls[imageIndex]} alt="" className="sliderImg" />
+            <div style={{width:"100%", height:"100%", display:"flex", overflow:"hidden"}}>
+                {imageUrls.map(url => (
+                    <img key={url} src={url} alt="" className="sliderImg" style={{translate:`${-100*imageIndex}%`}} />
+                ))}
+            </div>
             <button onClick={showPrevImage} className="sliderBtn1">&#8592;</button>
             <button onClick={showNextImage} className="sliderBtn2">&#8594;</button>
         </div>
