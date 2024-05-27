94da1effa7f5d11993cfc52b6785dd3152b0f80c
Samarth <samarth212@gmail.com>
2024-05-27 19:31:31 -0400

    route config

--- SpainTennis/src/App.jsx
+++ SpainTennis/src/App.jsx
@@ -5,18 +5,32 @@ import Clay from './Clay'
 import Rise from './Rise';
 import Borders from './Borders';
 import Conclusion from './Conclusion';
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
 
 function App() {
 
   return(
-    <>
-      <Hero></Hero>
-      <Legacy></Legacy>
-      <Clay></Clay>
-      <Rise></Rise>
-      <Borders></Borders>
-      <Conclusion></Conclusion>
-    </>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Hero></Hero>
+            <Legacy></Legacy>
+            <Clay></Clay>
+            <Rise></Rise>
+            <Borders></Borders>
+            <Conclusion></Conclusion>
+          </>
+          
+        }/>
+
+        
+      </Routes>
+
+    </Router>
+
   )
   
   
